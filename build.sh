9b57ea92aea8d466c343cd869498bb70d18ddeb9
Nataszha <naca.concepcion.coc@phinmaed.com>
2026-03-06 20:30:52 +0800

    Auto-create superuser in build script

--- build.sh
+++ build.sh
@@ -11,3 +11,6 @@ python manage.py collectstatic --no-input
 
 # Run database migrations
 python manage.py migrate
+
+# Create superuser automatically (skips if already exists)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='kres_admin').exists() or User.objects.create_superuser('kres_admin', 'naca.concepcion.coc@phinmaed.com', 'KresAdmin20')" | python manage.py shell
