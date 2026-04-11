5a8ec2257ba40f8a8f64db1adbf2dd6fa453f545
Nataszha <naca.concepcion.coc@phinmaed.com>
2026-04-11 14:01:12 +0800

    Update to Supabase database and fix superuser

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Create superuser automatically (skips if already exists)
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='kres_admin').exists() or User.objects.create_superuser('kres_admin', 'naca.concepcion.coc@phinmaed.com', 'KresAdmin20')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='krespresentsandcoffee@gmail.com').exists() or User.objects.create_superuser('admin', 'krespresentsandcoffee@gmail.com', 'january011821')" | python manage.py shell
